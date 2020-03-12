cb7e2cd66488acd093d781e5c54bb6afd5d7e3c6
Victor Gallet <victor.gallet@protonmail.ch>
2020-03-12 13:33:23 +0100

    test echo python

--- python/test_cowsay.py
+++ python/test_cowsay.py
@@ -1,8 +1,8 @@
 import cowsay
 import sys
 
-cowsay.tux("Python is fun")
+# cowsay.tux("Python is fun")
 
-cowsay.tux("Build ID {0}".format(sys.argv[1]))
+# cowsay.tux("Build ID {0}".format(sys.argv[1]))
 
-print("##vso[task.setvariable variable=MaVariable;isOutput=true]value")
+print("value")
